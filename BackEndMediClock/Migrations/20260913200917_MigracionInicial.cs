9743612829f6f85dc91eb48e3f334cc76f6492bf
Bascal Francisco <franbascal@gmail.com>
2026-09-13 17:10:15 -0300

    Migración inicial

---
+++ BackEndMediClock/Migrations/20260913200917_MigracionInicial.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEndMediClock.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dispositivos",
+                columns: table => new
+                {
+                    DispositivoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dispositivos", x => x.DispositivoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Alarmas",
+                columns: table => new
+                {
+                    AlarmaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DiaSemana = table.Column<int>(type: "int", nullable: false),
+                    NumeroAlarma = table.Column<int>(type: "int", nullable: false),
+                    Hora = table.Column<TimeOnly>(type: "time", nullable: false),
+                    DispositivoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alarmas", x => x.AlarmaId);
+                    table.ForeignKey(
+                        name: "FK_Alarmas_Dispositivos_DispositivoId",
+                        column: x => x.DispositivoId,
+                        principalTable: "Dispositivos",
+                        principalColumn: "DispositivoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    EventoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaHora = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    DispositivoId = table.Column<int>(type: "int", nullable: false),
+                    AlarmaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.EventoId);
+                    table.ForeignKey(
+                        name: "FK_Eventos_Alarmas_AlarmaId",
+                        column: x => x.AlarmaId,
+                        principalTable: "Alarmas",
+                        principalColumn: "AlarmaId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Eventos_Dispositivos_DispositivoId",
+                        column: x => x.DispositivoId,
+                        principalTable: "Dispositivos",
+                        principalColumn: "DispositivoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alarmas_DispositivoId_DiaSemana_NumeroAlarma",
+                table: "Alarmas",
+                columns: new[] { "DispositivoId", "DiaSemana", "NumeroAlarma" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Eventos_AlarmaId",
+                table: "Eventos",
+                column: "AlarmaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Eventos_DispositivoId",
+                table: "Eventos",
+                column: "DispositivoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+
+            migrationBuilder.DropTable(
+                name: "Alarmas");
+
+            migrationBuilder.DropTable(
+                name: "Dispositivos");
+        }
+    }
+}
